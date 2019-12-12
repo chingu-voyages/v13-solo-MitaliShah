fcaaeadcaea06dfe0afe8a007380d790db9da28a
Mitali Shah <mitalishaleenshah@gmail.com>
2019-12-12 13:29:42 -0800

    saving high scores array in local storage and converting array to string

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const Max_high_score = 5;
+
 finalScore.innerText = mostRecentScore;
 
 displayname.addEventListener("keyup", () => {
@@ -12,4 +16,18 @@ displayname.addEventListener("keyup", () => {
 saveHighScore = (e) => {
   console.log("clicked");
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: displayname.value
+  };
+  highScores.push(score);
+  highScores.sort((a,b) => {
+    return b.score - a.score;
+  });
+
+  highScores.splice(5);
+  
+  localStorage.setItem('highScore', JSON.stringify(highScores));
+  window.location.assign('/');
 }
